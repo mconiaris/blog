2a8abec0ddaba004e18a13ed4ca9002d3732148e
mconiaris <mconiaris@gmail.com>
2016-06-23 12:01:15 -0400

    Added pry to application for debugging.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    # binding.pry
     redirect_to article_path(@article)
   end
 
